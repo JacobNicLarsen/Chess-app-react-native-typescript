bd5c00b7b9fda3480debc38ecc9458fb142dffc4
Jacob Nicolai Larsen <jacob.larsen@inmeta.no>
2022-09-21 16:59:22 +0200

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState, useEffect } from "react"
+import { Profile } from "./types/profileLookup"
 
-export default function App() {
+
+function HomeScreen() {
+
+  const navigation = useNavigation()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="New Game"
+        onPress={() => navigation.navigate('Board')}
+      />
+      <></>
+      <Button
+        title="Lookup User"
+        onPress={() => navigation.navigate('Lookup')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function BoardScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+
+function LookupScreen() {
+  const [profile, setProfile] = useState<Profile>()
+
+  const fetchUserData = (username:string) => {
+    console.log("denne kjører")
+    fetch("https://api.chess.com/pub/player/" + username)
+    .then((response) => response.json())
+    .then((json) => {
+      setProfile(json)
+      console.log(json)
+    })
+  }
+
+  useEffect(() => {
+    fetchUserData("TorpedoShark")
+  },[])
+
+  const ProfileComponent = () => {
+    return (
+      <>
+        <Text>{profile?.player_id}</Text>
+        <Text>{profile?.['@id']}</Text>
+        <Text>{profile?.url}</Text>
+        <Text>{profile?.followers}</Text>
+        <Text>{profile?.country}</Text>
+        <Text>{profile?.last_online}</Text>
+        <Text>{profile?.joined}</Text>
+        <Text>{profile?.status}</Text>
+        <Text>{profile?.is_streamer}</Text>
+        <Text>{profile?.verified}</Text>
+      </>
+    )
+  }
+
+  return (
+    <View>
+      <Text>Here comes the user profile</Text>
+        <ProfileComponent /> 
+    </View>
+  )
+}
+
+
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Board" component={BoardScreen} />
+        <Stack.Screen name="Lookup" component={LookupScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+export default App;
+
---
+++ types/profileLookup.ts
@@ -0,0 +1,15 @@
+export type Profile = {
+        player_id: number;
+        "@id": string;
+        url: string;
+        username: string;
+        followers: number;
+        country: string;
+        last_online: number;
+        joined: number;
+        status: string;
+        is_streamer: boolean;
+        verified: boolean;
+    }
+
+    export default Profile
